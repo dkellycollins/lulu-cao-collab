049099e2b910c180261e6b051759b494e740900e
lulu <lulu.cao.her@gmail.com>
2025-05-18 22:35:44 -0500

    minor updates for app controller

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getFeaturedBlogs(): string {
+  hello(): string {
     return "VibeReads - A list of featured blogs coming soon..."
   }
 }
