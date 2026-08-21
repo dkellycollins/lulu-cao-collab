95ddb3a3ed8f41827980cfcf870aef68ac0b4335
lulu <lulu.cao.her@gmail.com>
2026-08-21 13:04:28 -0700

    migrate from sqlite to postgresql since sqlite doesn't support concurrent writes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,22 +4,26 @@ import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { BlogModule } from '../blog/blog.module';
 import { Blog } from '../blog/entities/blog.entity';
-// import { BookModule } from 'src/book/book.module';
-// import { Book } from 'src/book/entities/book.entity';
+// import { BookModule } from '../book/book.module';
+// import { Book } from '../book/entities/book.entity';
 import { User } from '../user/entities/user.entity';
 import { UserModule } from '../user/user.module';
-// import { ReviewModule } from 'src/review/review.module';
-// import { Review } from 'src/review/entities/review.entity';
+// import { ReviewModule } from '../review/review.module';
+// import { Review } from '../review/entities/review.entity';
 import { File } from '../file/entities/file.entity';
 import { FileModule } from '../file/file.module';
 import migrations from '../migrations';
-import { RedisModule } from 'src/redis/redis.module';
+import { RedisModule } from '../redis/redis.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({ // Integrate with database
-      type: 'sqlite',
-      database: 'db',
+      type: 'postgres',
+      host: process.env.DB_HOST ?? 'localhost',
+      port: parseInt(process.env.DB_PORT ?? '5432', 10),
+      username: process.env.DB_USERNAME ?? 'vibereads',
+      password: process.env.DB_PASSWORD ?? 'vibereads',
+      database: process.env.DB_DATABASE ?? 'vibereads',
       entities: [Blog, User, File],
       migrations: migrations,
       migrationsRun: true,
